7693194570f7c93ff8ce0b2617a3d70563f5aa50
Carpe Necopinum <CarpeNecopinum@users.noreply.github.com>
2020-03-04 15:36:04 +0100

    Document ASCII IO

--- src/io_ascii.jl
+++ src/io_ascii.jl
@@ -1,20 +1,39 @@
-import ZipFile
-
-function saveASCII(f::IO, properties::Vector...)
-    @assert length(properties) > 0
-    for i in 1:length(properties[1])
-        join(f, Base.Iterators.flatten(p[i] for p in properties), " ")
+function saveASCII(f::IO, attributes::Vector...)
+    @assert length(attributes) > 0
+    for i in 1:length(attributes[1])
+        join(f, Base.Iterators.flatten(p[i] for p in attributes), " ")
         println(f)
     end
 end
 
-function saveASCII(filename::AbstractString, properties::Vector...)
-    @assert length(properties) > 0
+function saveASCII(filename::AbstractString, attributes::Vector...)
+    @assert length(attributes) > 0
 
     open(filename, "w") do f
-        saveASCII(f, properties...)
+        saveASCII(f, attributes...)
     end
 end
+
+"""
+    saveASCII(::Union{AbstractString,IO}, attributes::Vector...)
+
+Store the `attributes` as XYZ / ASCII point cloud file.
+The format of the lines will adapt to the types of the attributes provided.
+
+```jldoctest
+io = IOBuffer()
+
+Clouds.saveASCII(io, cloud.positions)
+Text(String(take!(io)))
+
+#output
+
+0.0 0.0 0.0
+0.0 0.0 0.0
+0.0 0.0 0.0
+```
+"""
+saveASCII
 
 
 function loadASCII(f::IO, types::Type...)
@@ -51,3 +70,17 @@ end
 
 loadASCII(filename::AbstractString; attributes...) =
     open(f -> loadASCII(f; attributes...), filename)
+
+
+"""
+    loadASCII(f::Union{AbstractString,IO}; attributes...)
+
+Loads `f` as PointCloud. The attributes to load are provided as keyword arguments, where the
+key corresponds to the attribute name and the value corresponds to the eltype of the Vector.
+
+    loadASCII(f::Union{AbstractString,IO}, types::Type...)
+
+Loads `f` as a Tuple with one element per type in `types`.
+
+"""
+loadASCII
